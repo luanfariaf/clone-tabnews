b40adf67204b24e17a1945085d65c2717bc4ae42
Luan Faria <luuan.fariaf@gmail.com>
2024-08-13 15:53:21 -0300

    Add `index` page

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', color: '#3d3d3d'}}>
+      <h1>Hello there 👋🏼.</h1>
+    </div>
+  )
+}
+
+export default Home;
