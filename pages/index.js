bf329527c8968da03ab71800d4b05aa727db19f7
Luan Faria <luuan.fariaf@gmail.com>
2024-08-22 12:26:04 -0300

    update .gitignore

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', color: '#3d3d3d'}}>
-      <h1>Hello there 👋🏼.</h1>
+      <h1>Hello there 👋🏼</h1>
     </div>
   )
 }
