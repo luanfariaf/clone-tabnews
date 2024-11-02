032e8d62b29c1084dea4eedc627c3ea98e904200
Luan Faria <luuan.fariaf@gmail.com>
2024-11-01 23:12:09 -0300

    add `.editorconfig`.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
 function Home() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', color: '#3d3d3d'}}>
+    <div
+      style={{ display: "flex", justifyContent: "center", color: "#3d3d3d" }}
+    >
       <h1>Hello there 👋🏼</h1>
     </div>
-  )
+  );
 }
 
 export default Home;
