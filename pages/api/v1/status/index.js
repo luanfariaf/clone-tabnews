697f34b4cf4882646d1dd472c3da7ba9cd9ff13e
Luan Faria <luuan.fariaf@gmail.com>
2025-04-01 19:54:20 -0300

    finish `api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,27 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ status: "ok" });
+  const updatedAt = new Date().toISOString();
+  const version = await database.query("SHOW server_version;");
+  const databaseName = process.env.POSTGRES_DB;
+  const result = await database.query({
+    text: `
+      SELECT
+      (SELECT setting::int FROM pg_settings WHERE name = 'max_connections') AS max_connections,
+      (SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1) AS opened_connections`,
+    values: [databaseName]
+  });
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: version.rows[0].server_version,
+        max_connections: result.rows[0].max_connections,
+        opened_connections: result.rows[0].opened_connections,
+      },
+    },
+  });
 }
 
 export default status;
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,19 @@
 test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  const version = responseBody.dependencies.database.version;
+  expect(version).toBe("16.6");
+
+  const maxConnections = responseBody.dependencies.database.max_connections;
+  expect(maxConnections).toBeDefined();
+
+  const openConnections = responseBody.dependencies.database.opened_connections;
+  expect(openConnections).toBe(1);
 });
