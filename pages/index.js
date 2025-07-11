cf3effc375ee96d9faeb04107839b56378d43c5a
Luan Faria <luuan.fariaf@gmail.com>
2025-07-10 22:54:51 -0300

    change  title

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ function Home() {
     <div
       style={{ display: "flex", justifyContent: "center", color: "#3d3d3d" }}
     >
-      <h1>Hello there 👋🏼</h1>
+      <h1>Hello there 👋🏼, we are learning with <strong>curso.dev</strong></h1>
     </div>
   );
 }
